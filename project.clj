748de210a25e7707651cf0bf449df7d7361fc34b
Travis Vachon <travis.vachon@gmail.com>
2011-01-25 10:38:19 -0500

    project.clj was a mess
    dependencies broken, some didn't exist - just a disaster. better now!

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
-(defproject lein-cuke "0.0.1-SNAPSHOT"
+(defproject lein-cuke "0.0.2"
   :description "Cucumber runner for leiningen"
-  :repositories [["cukes" "http://cukes.info/maven"]
-                 ["clojars" "http://clojars.org/repo"]]
-  :dependencies [[leiningen "1.0.0-SNAPSHOT"]
-                 [lein-clojars "0.5.0-SNAPSHOT"]
-                 [lein-cuke "0.0.1-SNAPSHOT"]
-                 [cuke4duke "0.2.3"]])
+  :repositories {"cukes" "http://cukes.info/maven"
+                 "clojars" "http://clojars.org/repo"}
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [cuke4duke "0.4.3"
+                  :exclusions [org.clojure/clojure
+                               org.clojure/clojure-contrib]]]
+  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]])
 
